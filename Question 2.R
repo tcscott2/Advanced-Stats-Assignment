8c94a0260ce1248c03ff92bd057a83b278172cc9
DDamCodes <53192487+DDamCodes@users.noreply.github.com>
2021-08-28 09:53:59 -0500

    Create Question 2.R
    Initial commit

---
+++ Question 2.R
@@ -0,0 +1,11 @@
+# The associate dean of a business school was looking for ways to improve the quality of the
+# applicants to its MBA program. In particular she wanted to know whether the undergraduate
+# degree of applicants differed among her school and the three nearby universities with MBA
+# programs. She sampled 100 applicants of her program and an equal number from each of the
+# other universities. She recorded their undergraduate degree (1 = BA, 2 = B.Eng, 3 = BBA, and 4
+#                                                              =Other) as well as the university (codes 1, 2, 3, and 4).
+# Refer to the MBA.rds dataset and answer the below questions.
+# a. What type of data is this?
+#   b. Using R, tabulate and graph the relationship between the applicant’s undergraduate degree
+# and the university that applied to.
+# c. Is the undergraduate degree and the university each person applied to appear to be related?

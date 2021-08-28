6c8740d6c28af449b00cbf8ea2b13fb8db1c3820
DDamCodes <53192487+DDamCodes@users.noreply.github.com>
2021-08-28 12:27:02 -0500

    Q2 complete soln
    graph needs polishing

--- Question 2.R
+++ Question 2.R
@@ -10,6 +10,11 @@
 # and the university that applied to.
 # c. Is the undergraduate degree and the university each person applied to appear to be related?
 
+# install.packages("magrittr") # package installations are only needed the first time you use it
+# install.packages("dplyr")    # alternative installation of the %>%
+library(magrittr) # needs to be run every time you start R and want to use %>%
+library(dplyr)    # alternatively, this also loads %>%
+
 #comment out if not mac and point to directory
 setwd("~/Documents/Adv Stat/Assignments")
 
@@ -25,10 +30,45 @@ MBA$Degreelabel <- factor(MBA$Degree,
                                            "Other"))
 levels(MBA$Degreelabel)
 
-MBA$Universitylabel<- factor(MBA$University,
-                                 levels=c(1,2,3,4),
-                                 labels=c("G&M",
-                                          "Post",
-                                          "Star",
-                                          "Sun")) 
-levels(newsread$Newspaperlabel)
+# MBA$Universitylabel<- factor(MBA$University,
+#                                  levels=c(1,2,3,4),
+#                                  labels=c("G&M",
+#                                           "Post",
+#                                           "Star",
+#                                           "Sun")) 
+# levels(newsread$Newspaperlabel)
+
+##Table of Frequencies
+n=nrow(MBA); n #Total number of observations
+addmargins(with(MBA,table(Degreelabel,University)))
+
+#Row-relative Frequencies
+with(MBA,table(Degreelabel,University)) %>% 
+  prop.table(margin=1) %>% #value of each cell is divided by the sum of the row cells
+  round(2)
+
+#Column-relative Frequencies
+with(MBA,table(Degreelabel,University)) %>% 
+  prop.table(margin=2) %>% #each cell is divided by the sum of the column cells.
+  round(2)
+
+#Overall-relative Frequencies
+with(MBA,table(Degreelabel,University)) %>% 
+  prop.table(margin=) %>% #default is "margin=", which calculates proportions over the entire table.
+  round(2)
+
+#----------Graphing the Relationship b/w two Nominal Variables------------------
+#Comparison Bar Graph
+count<- table(MBA$Degreelabel,MBA$University); count
+barplot(count, 
+        main="Two-dimentional Bar Chart",
+        xlab="Occupation", 
+        ylab="Frequency", 
+        col=c("darkblue","red","green", "yellow"),
+        cex=1.2, cex.axis=1.2,cex.lab=1.2,
+        legend=rownames(count), beside=TRUE)
+
+
+
+
+
